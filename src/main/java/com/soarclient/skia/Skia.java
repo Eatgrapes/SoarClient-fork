47418f550bee319432b34165b94571d9a6b162a2
LazyChara <admin@onechara.eu.org>
2026-01-24 15:21:55 +0800

    代投

--- src/main/java/com/soarclient/skia/Skia.java
+++ src/main/java/com/soarclient/skia/Skia.java
@@ -25,10 +25,50 @@ import io.github.humbleui.types.Rect;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.Window;
 import net.minecraft.util.Identifier;
+import com.mojang.blaze3d.platform.GlConst;
+import com.mojang.blaze3d.systems.RenderSystem;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL33;
+import java.util.function.Consumer;
 
 public class Skia {
 
 	private static final ImageHelper imageHelper = new ImageHelper();
+
+	public static void draw(Consumer<Canvas> drawingLogic) {
+		if (SkiaContext.getCanvas() == null) return;
+		RenderSystem.pixelStore(GlConst.GL_UNPACK_ROW_LENGTH, 0);
+		RenderSystem.pixelStore(GlConst.GL_UNPACK_SKIP_PIXELS, 0);
+		RenderSystem.pixelStore(GlConst.GL_UNPACK_SKIP_ROWS, 0);
+		RenderSystem.pixelStore(GlConst.GL_UNPACK_ALIGNMENT, 4);
+		RenderSystem.clearColor(0f, 0f, 0f, 0f);
+		if (SkiaContext.getContext() != null) SkiaContext.getContext().resetGLAll();
+		Canvas canvas = SkiaContext.getCanvas();
+		if (canvas != null) {
+			drawingLogic.accept(canvas);
+		}
+		if (SkiaContext.getContext() != null) SkiaContext.getContext().flush();
+		GL33.glBindSampler(0, 0);
+		RenderSystem.disableBlend();
+		GL11.glDisable(GL11.GL_BLEND);
+		RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+		RenderSystem.blendEquation(GL33.GL_FUNC_ADD);
+		GL33.glBlendEquation(GL33.GL_FUNC_ADD);
+		RenderSystem.colorMask(true, true, true, true);
+		GL11.glColorMask(true, true, true, true);
+		RenderSystem.depthMask(true);
+		GL11.glDepthMask(true);
+		RenderSystem.disableScissor();
+		GL11.glDisable(GL11.GL_SCISSOR_TEST);
+		GL11.glDisable(GL11.GL_STENCIL_TEST);
+		RenderSystem.disableDepthTest();
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		RenderSystem.activeTexture(GL13.GL_TEXTURE0);
+		RenderSystem.disableCull();
+	}
 
 	public static void drawRect(float x, float y, float width, float height, Color color) {
 		getCanvas().drawRect(Rect.makeXYWH(x, y, width, height), getPaint(color));
